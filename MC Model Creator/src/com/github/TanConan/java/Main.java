314cff5f6f632273cc2c3ddcb4d4f00646605ec6
TanConan <>
2019-01-22 02:08:31 +0100

    added launch

--- MC Model Creator/src/com/github/TanConan/java/Main.java
+++ MC Model Creator/src/com/github/TanConan/java/Main.java
@@ -1,10 +1,19 @@
 package com.github.TanConan.java;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
 		
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+		
 	}
 
 }
